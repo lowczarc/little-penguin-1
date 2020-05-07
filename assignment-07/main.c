4cd97b4bb90e1774139e7d4525098b8eeb0f3ce2
Lancelot Owczarczak <contact@lancelot.life>
2020-05-07 16:00:54 +0200

    add security to id. add jiffies and foo to assignment-07

--- assignment-07/main.c
+++ assignment-07/main.c
@@ -2,16 +2,30 @@
 #include <linux/kernel.h>
 #include <linux/debugfs.h>
 #include <linux/init.h>
+#include <linux/jiffies.h>
+#include <linux/slab.h>
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Lancelot Owczarczak <contact@lancelot.life>");
-MODULE_DESCRIPTION("DebugFS test");
+#define LOGIN "lowczarc"
+#define LOGIN_SIZE 8
 
 static ssize_t id_file_write(struct file *filp, const char *buffer,
 			     size_t length, loff_t * offset);
 
 static ssize_t id_file_read(struct file *filp, char *buffer,
 			    size_t length, loff_t * offset);
+
+static ssize_t jiffies_file_read(struct file *filp, char *buffer,
+				 size_t length, loff_t * offset);
+
+static ssize_t foo_file_read(struct file *filp, char *buffer,
+			     size_t length, loff_t * offset);
+
+static ssize_t foo_file_write(struct file *filp, const char *buffer,
+			      size_t length, loff_t * offset);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Lancelot Owczarczak <contact@lancelot.life>");
+MODULE_DESCRIPTION("DebugFS test");
 
 static struct dentry *Dfortytwo;
 
@@ -23,13 +37,17 @@ static struct file_operations id_fops = {
 
 static struct dentry *Djiffies;
 static struct file_operations jiffies_fops = {
-	// TODO: add jiffies operations
+	.read = jiffies_file_read,
 };
 
 static struct dentry *Dfoo;
 static struct file_operations foo_fops = {
-	// TODO: add foo operations
+	.write = foo_file_write,
+	.read = foo_file_read,
 };
+
+static char *Foo_stored_string = NULL;
+static size_t Foo_stored_size = 0;
 
 static int error_cleanup(void)
 {
@@ -79,27 +97,107 @@ static int __init hello_init(void)
 static void __exit hello_cleanup(void)
 {
 	debugfs_remove_recursive(Dfortytwo);
+
+	if (Foo_stored_string != NULL)
+		kfree(Foo_stored_string);
+
 	printk(KERN_INFO "Cleaning up DebugFS test module.\n");
 }
 
 static ssize_t id_file_read(struct file *filp, char *buffer,
 			    size_t length, loff_t * offset)
 {
-	int retval = length < 8 ? length : 8;
+	int retval;
+	size_t size_read;
 
-	strncpy(buffer, "lowczarc", retval);
+	if (*offset >= LOGIN_SIZE) {
+		return 0;
+	}
 
-	return retval;
+	size_read =
+	    length < LOGIN_SIZE - *offset ? length : LOGIN_SIZE - *offset;
+
+	retval = copy_to_user(buffer, LOGIN + *offset, size_read);
+
+	if (retval < 0)
+		return retval;
+
+	size_read -= retval;
+	*offset += size_read;
+
+	return size_read;
 }
 
 static ssize_t id_file_write(struct file *filp, const char *buffer,
 			     size_t length, loff_t * offset)
 {
-	if (length != 8 || strncmp(buffer, "lowczarc", 8)) {
+	char local_buffer[LOGIN_SIZE];
+
+	if (length != LOGIN_SIZE)
+		return -EINVAL;
+
+	if (copy_from_user(local_buffer, buffer, LOGIN_SIZE))
+		return -EINVAL;
+
+	if (memcmp(local_buffer, LOGIN, LOGIN_SIZE))
+		return -EINVAL;
+
+	return LOGIN_SIZE;
+}
+
+static ssize_t jiffies_file_read(struct file *filp, char *buffer,
+				 size_t length, loff_t * offset)
+{
+	int size = snprintf(buffer, length, "%lu\n", jiffies);
+
+	return size > length ? length : size;
+}
+
+// TODO: add mutex to avoid use after free when read and write at the same time
+
+static ssize_t foo_file_read(struct file *filp, char *buffer,
+			     size_t length, loff_t * offset)
+{
+	int retval;
+	size_t read_size;
+	if (Foo_stored_string == NULL)
+		return 0;
+
+	read_size = (Foo_stored_size - *offset) >
+	    length ? length : (Foo_stored_size - *offset);
+
+	retval = copy_to_user(buffer, Foo_stored_string + *offset, read_size);
+
+	if (retval < 0)
+		return retval;
+
+	read_size -= retval;
+
+	*offset += read_size;
+
+	return read_size;
+}
+
+static ssize_t foo_file_write(struct file *filp, const char *buffer,
+			      size_t length, loff_t * offset)
+{
+	char *new_string;
+
+	if (length > PAGE_SIZE)
+		return -EINVAL;
+
+	new_string = kmalloc(length, GFP_KERNEL);
+
+	if (copy_from_user(new_string, buffer, length)) {
+		kfree(new_string);
 		return -EINVAL;
 	}
 
-	return 8;
+	kfree(Foo_stored_string);
+	Foo_stored_string = new_string;
+	Foo_stored_size = length;
+
+	return length;
 }
 
 module_init(hello_init);
