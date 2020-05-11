389c3ee37faaf8393887485d40a93def16a6043b
Lancelot Owczarczak <contact@lancelot.life>
2020-05-11 21:01:45 +0200

    remove printk

--- assignment-08/fix_style.c
+++ assignment-08/fix_style.c
@@ -35,12 +35,8 @@ static int __init myfd_init(void)
 	retval = misc_register(&myfd_device);
 
 	str[0] = 0;
-	if (retval) {
-		printk(KERN_ERR "misc_register returned: %d\n", retval);
-		return retval;
-	}
 
-	return 0;
+	return retval;
 }
 
 static void __exit myfd_cleanup(void)
@@ -60,12 +56,8 @@ static ssize_t myfd_read(struct file *fp, char __user * user, size_t size,
 
 	rev_str = kmalloc(PAGE_SIZE, GFP_KERNEL);
 
-	printk(KERN_WARNING "str_size: %ld\n", str_size);
-
-	for (t = str_size - 1, i = 0; t >= 0; t--, i++) {
-		printk(KERN_ERR "t: %ld", t);
+	for (t = str_size - 1, i = 0; t >= 0; t--, i++)
 		rev_str[i] = str[t];
-	}
 
 	rev_str[i] = 0;
 
@@ -81,9 +73,9 @@ static ssize_t myfd_write(struct file *fp, const char __user * user,
 {
 	ssize_t res;
 
-	if (size + 1 >= PAGE_SIZE) {
+	if (size + 1 >= PAGE_SIZE)
 		return -1;
-	}
+
 	res = simple_write_to_buffer(str, size, offs, user, size);
 	str[res + 1] = 0;
 
